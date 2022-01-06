4308d21ed2162c38c31c6cdaed8a84e2d2cb750e
solarjoni <solarjoni@gmail.com>
2022-01-06 13:39:39 +0200

    1

---
+++ tests/test.file.js
@@ -0,0 +1,16 @@
+module.exports = {
+    'Filling out form': (client) => {
+        client
+            .url('https://www.ultimateqa.com/filling-out-forms')
+            .waitForElementVisible('body', 1000)
+            .assert.title('Filling Out Forms | Ultimate QA')
+            .waitForElementVisible('#et_pb_contact_name_0')
+            .updateValue('#et_pb_contact_name_0', 'John')
+            .updateValue('#et_pb_contact_message_0', 'this is a paragraph')
+            .pause(3000)
+            .click('.et_pb_contact_submit')
+            .waitForElementVisible('#et_pb_contact_form_0')
+            //.pause(3000)
+            .expect.element('#et_pb_contact_form_0').text.to.equal('Thanks for contacting us')
+    }
+};
