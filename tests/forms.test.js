8f76eb7e88052ec13e0259c0894c6cd82f1aa4c0
solarjoni <solarjoni@gmail.com>
2022-01-13 16:15:02 +0200

    Page Object

--- tests/forms.test.js
+++ tests/forms.test.js
@@ -9,11 +9,27 @@ module.exports = {
         await browser
         .updateValue('#et_pb_contact_name_0', 'John Doe')
         .updateValue('#et_pb_contact_message_0', 'Lorem Ipsum')
-        .pause(100) //to make it slower so can submit the form
+        .pause(1000) //to make it slower so can submit the form
         .submitForm('#et_pb_contact_form_0 form')
         //.click('.et_pb_contact_submit')
         //.waitForElementVisible('#et_pb_contact_form_0')
         .expect.element('#et_pb_contact_form_0').text.to.be.equal('Thanks for contacting us')
+    },
+
+    'Should successfully fill out the left form using Page Object': (browser) => {
+        const FormPage = browser.page.formPage()
+        const rightForm = FormPage.section.rightForm
+
+        FormPage
+        .navigate()
+        .verify.title('Filling Out Forms | Ultimate QA')
+        //Error while running .getElementText() protocol action: stale element reference: element is not attached to the page document
+        //(Session info: chrome=97.0.4692.71)
+        .enterNameAndMessage('John Doe', 'Lorem Ipsum')
+        .submitLeftForm()
+        .verify.containsText('@lblLeftFormMessage:nth-child(1)', 'Thanks for contacting us') //:nth-child(1) could be in definition also
+        
+        rightForm.expect.element('@txtName').to.be.visible
     },
 
     'Should verify error message when left form is filled no fully': async (browser) => {
