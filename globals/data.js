ac530de50b3f727a23391b3bdd1bb738b600651f
solarjoni <solarjoni@gmail.com>
2022-01-14 15:43:22 +0200

    Saving Screenshot

--- globals/data.js
+++ globals/data.js
@@ -3,18 +3,21 @@ module.exports = [
         testCase: 'Should verify that thr name error message is shown',
         name: '',
         message: 'Lorem Ipsum',
+        screenFileName: './screens/err_name.png',
         expected: 'Please, fill in the following fields:\nName'
     },
     {
         testCase: 'Should verify that the message roor is shown',
         name: 'John Doe',
         message: '',
+        screenFileName: './screens/err_msg.png',
         expected: 'Please, fill in the following fields:\nMessage'
     },
     {
         testCase: 'Should verify that the name and message errors are shown',
         name: '',
         message: '',
+        screenFileName: './screens/err.png',
         expected: 'Please, fill in the following fields:\nName\nMessage'
     }
 
--- tests_mocha/mocha.dd.form.test.js
+++ tests_mocha/mocha.dd.form.test.js
@@ -1,7 +1,7 @@
 const data = require('../globals/data')
 
 describe('Forms Suite', () => {
-    data.forEach(({testCase, name, message, expected}) => {
+    data.forEach(({testCase, name, message, screenFileName, expected}) => {
         it(testCase, (browser) => {
             const FormPage = browser.page.formPage()
 
@@ -11,6 +11,7 @@ describe('Forms Suite', () => {
                 .enterNameAndMessage(name, message)
                 .submitForm('@form')
                 .verify.containsText('@lblLeftFormMessage', expected)
+                .saveScreenshot(screenFileName)
         })       
 
     })
