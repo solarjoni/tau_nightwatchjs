bfebd747f701009cc348f24d6795b7e269b9e877
solarjoni <solarjoni@gmail.com>
2022-01-14 14:26:35 +0200

    Mocha Data Driven

---
+++ tests_mocha/mocha.forms.test.js
@@ -0,0 +1,13 @@
+describe('Forms Suite', () => {
+    it('Should fill out form successfully', (browser) => {
+        const FormPage = browser.page.formPage()
+
+        FormPage
+            .navigate()
+            //throws stale element reference error
+            .assert.title('Filling Out Forms | Ultimate QA')
+            .enterNameAndMessage('John Doe', 'Lorem Ipsum')
+            .submitLeftForm()
+            .verify.containsText('@lblLeftFormMessage', 'Thanks for contacting us')
+    })
+})
